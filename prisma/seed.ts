040c7aa65f073cc45fa65ab734af9f53ddd4a9bc
guitarkeegan <keegananglim@gmail.com>
2023-03-26 01:30:38 -0700

    working on gatherData script to build db models

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import { db } from "@/lib/db";
+import { CORE_TYPE } from "@prisma/client";
+
+
+async function main() {
+  
+  
+}
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
