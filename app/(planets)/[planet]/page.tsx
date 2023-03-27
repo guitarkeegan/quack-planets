ea8a9b223090561d33cd1455ecf0f8a58933084c
guitarkeegan <keegananglim@gmail.com>
2023-03-27 11:37:44 -0700

    refactor routes for plantets

---
+++ app/(planets)/[planet]/page.tsx
@@ -0,0 +1,16 @@
+import Jupiter from "@/components/Jupiter";
+
+export default function Planet({params}: {
+    params: { planet: string };
+  }){
+
+    switch (params.planet){
+        case "jupiter":
+            return <Jupiter />
+        default:
+            return <Jupiter />
+    }
+
+
+    
+}
--- components/Jupiter.tsx
+++ components/Jupiter.tsx
@@ -1,6 +1,6 @@
-import styles from "./styles.module.css"
 
 export default function Jupiter(){
+    
     return (
         <section>
             <div className="text-white">
@@ -8,8 +8,9 @@ export default function Jupiter(){
                 <h2>The largest planet in the solar system, a gas giant which is the fifth in order from the sun and one of the brightest objects in the night sky.</h2>
             </div>
             <div id="jupiter-wrapper" className=" flex justify-center">
-                <div id="jupiter" className="md:w-[700px] md:h-[700px] rounded-full" style={{background: "linear-gradient(140deg, rgb(106, 97, 78), rgb(118, 121, 120), rgb(100, 81, 67), rgb(138, 119, 82), rgb(140, 140, 120), #fdba74)"}}></div>
+                <div id="jupiter" className="md:w-[700px] md:h-[700px] rounded-full" style={{background: "linear-gradient(140deg, #ffedd5, #fdba74, #f97316, #fdba74, #ffedd5)"}}></div>
             </div>
         </section>
     )
 }
+
