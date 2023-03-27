e331a28f433938a682d76bc3496d19c6161ad281
guitarkeegan <keegananglim@gmail.com>
2023-03-27 12:26:26 -0700

    add responsiveness

--- app/(planets)/[planet]/page.tsx
+++ app/(planets)/[planet]/page.tsx
@@ -1,7 +1,5 @@
 import Jupiter from "@/components/Jupiter";
 
-// TODO: make db call here and pass props to planet component
-
 export default function Planet({ params }: { params: { planet: string } }) {
   switch (params.planet) {
     case "jupiter":
