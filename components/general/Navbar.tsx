cae60ae8862199c1644370e0850fefe8e0d92b35
Rick Rieger <rickrieger36251@gmail.com>
2023-03-31 06:28:39 -0400

    Added solar system animation to home page

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,32 +1,29 @@
-import { getAllPlanets } from "@/utils";
-import Link from "next/link";
+import { getAllPlanets } from '@/utils';
+import Link from 'next/link';
 
-interface Route{
-    href: string,
-    name: string
+interface Route {
+  href: string;
+  name: string;
 }
 
-const getRoutes = async ()=>{
-  const routes : Route[] = [{href:'/', name:'Home'}];
-  const planets = await  getAllPlanets();
+const getRoutes = async () => {
+  const routes: Route[] = [{ href: '/', name: 'Home' }];
+  const planets = await getAllPlanets();
 
-  planets.map(planet=>{
-   let route : Route = {href: `/${planet.name}`, name:planet.name}
-   routes.push(route);
-
+  planets.map((planet) => {
+    let route: Route = { href: `/${planet.name}`, name: planet.name };
+    routes.push(route);
   });
 
   return routes;
-}
-
-
+};
 
 const Navbar = async () => {
-  const  routes : Route[]=  await getRoutes();
+  const routes: Route[] = await getRoutes();
   return (
-    <div className="bg-white p-4 ">
+    <div className='bg-white p-4 w-full'>
       {routes.map((route, index) => (
-        <Link className="p-2" key={index} href={`${route.href}`}>
+        <Link className='p-2' key={index} href={`${route.href}`}>
           {route.name}
         </Link>
       ))}
--- components/planets/PlanetDisplay.tsx
+++ components/planets/PlanetDisplay.tsx
@@ -3,58 +3,51 @@
  * todo display on planets pages.
  */
 
-import { FC } from "react";
+import { FC } from 'react';
 import styles from './styles.module.css';
 type PlanetComponentProps = {
   name: string;
   description: string;
 };
-// todo add props for a style object. 
-const PlanetDisplay : FC<PlanetComponentProps> = ({ name, description }) => {
-  
+// todo add props for a style object.
+const PlanetDisplay: FC<PlanetComponentProps> = ({ name, description }) => {
   return (
-    <section className="sm:grid sm:grid-cols-2 mt-24" >
-      <div className="text-white flex items-start justify-evenly flex-col px-6">
+    <section className='sm:grid sm:grid-cols-2 mt-24'>
+      <div className='text-white flex items-start justify-evenly flex-col px-6'>
         <h1 className={styles[name.toLocaleLowerCase()]}>{name}</h1>
-        <h2 className="text-2xl">{description}</h2>
+        <h2 className='text-2xl'>{description}</h2>
       </div>
       <div
-        id="planet-wrapper"
-        className="sm:mt-0 mt-12 flex justify-center items-center"
+        id='planet-wrapper'
+        className='sm:mt-0 mt-12 flex justify-center items-center'
       >
-        <div
-          id="jupiter"
-          className={planets(name.toLowerCase())}
-        ></div>
+        <div id='jupiter' className={planets(name.toLowerCase())}></div>
       </div>
     </section>
   );
 };
 
 const planets = (name: string) => {
-  switch (name){
-    case "mercury":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "venus":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "earth":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "mars":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "jupiter":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "saturn":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "uranus":
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
-    case "neptune":
-        return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
+  switch (name) {
+    case 'mercury':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'venus':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'earth':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'mars':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'jupiter':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'saturn':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'uranus':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
+    case 'neptune':
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
     default:
-      return "lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full"
+      return 'lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] bg-blue-400 rounded-full';
   }
-  
-}
-
-
+};
 
 export default PlanetDisplay;
