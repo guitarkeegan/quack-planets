1102902b534ab56b21ef6b9084db16faf3cf6a50
guitarkeegan <keegananglim@gmail.com>
2023-03-31 14:23:06 -0700

    add gradient to venus

---
+++ components/planets/MercuryDisplay.tsx
@@ -0,0 +1,8 @@
+export default function MercuryDisplay(){
+    return (
+        <div id="mercury" className="lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] rounded-full">
+            <div className="h-[4px] w-[4px]"></div>
+        </div>
+        
+    )
+}
