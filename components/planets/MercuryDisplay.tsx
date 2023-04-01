e443681a9f4e498239a5c167c8dabc6bc9e5a50b
guitarkeegan <keegananglim@gmail.com>
2023-04-01 08:40:13 -0700

    update mercury style

--- components/planets/MercuryDisplay.tsx
+++ components/planets/MercuryDisplay.tsx
@@ -4,10 +4,15 @@ export default function MercuryDisplay(){
 
     return (
         <div id="mercury" className="lg:w-[30rem] lg:h-[30rem] md:w-[400px] md:h-[400px] w-[280px] h-[280px] rounded-full flex flex-col items-center">
-            <div className="h-[10px] w-[10px] rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
+            <div className="h-[10px] w-[10px] blur-sm opacity-50 rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
+            <div className="h-[10px] w-[10px] ml-[20px] blur-sm opacity-50 rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
+            <div className="h-[10px] blur-sm opacity-50 w-[10px] mr-[30px] rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
             {craterArr.map((x, i)=>(
-                <div style={{marginRight: `${Math.floor(Math.random() * 260)}px`, marginLeft: `${Math.floor(Math.random() * 260)}px`}} key={i} className={`h-[10px] w-[10px] rounded-full bg-slate-300 shadow-lg shadow-slate-200`}></div>
+                <div style={{marginRight: `${Math.floor(Math.random() * 260)}px`, marginLeft: `${Math.floor(Math.random() * 260)}px`}} key={i} className={`h-[10px] blur-sm w-[10px] opacity-50 rounded-full bg-slate-300 shadow-lg shadow-slate-200`}></div>
             ))}
+            <div className="h-[10px] w-[10px] blur-sm opacity-50 rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
+            <div className="h-[10px] w-[10px] ml-[20px] blur-sm opacity-50 rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
+            <div className="h-[10px] blur-sm opacity-50 w-[10px] mr-[30px] rounded-full bg-slate-300 shadow-lg shadow-slate-200"></div>
         </div>
         
     )
