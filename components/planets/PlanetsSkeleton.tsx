9d9f3b47db3d707f02fe9df63b88b2cd008bbedc
guitarkeegan <keegananglim@gmail.com>
2023-04-01 11:44:23 -0700

    move spinner to middle

--- components/planets/PlanetsSkeleton.tsx
+++ components/planets/PlanetsSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function PlanetsSkeleton() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-[400px]">
       <div className="text-white text-4xl">loading...</div>
       <div className="h-8 w-8 border-4 border-dashed rounded-full animate-spin border-violet-400"></div>
     </div>
